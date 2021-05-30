6dc1328781c08dc1a10df11ed43f49ad64c0c5d4
Gerard Klijs <g.klijs@gmail.com>
2021-05-30 10:38:12 +0200

    simplify poc so it works

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # ksqlDB GraphQL poc
 
+![Overview](overview-ksql.png)
+
 Setup to serve as proof of concept in using Kafka with ksqlDB in combination with the query language GraphQL.
 
-`js-graphql-endpoint` runs on [http://localhost:4000](http://localhost:4000) (once it can handle references)
+`js-graphql-endpoint` runs on [http://localhost:4000](http://localhost:4000)
 
 Open interactive ksql promt: `docker exec -it ksqldb ksql http://ksqldb:8088`
 
-The `data-producer` is updating every 5 seconds, which causes all the persons in the update to update.
+The `rust-data-creator` is sending a new person every 5 seconds.
 
-See the results: `kafka-protobuf-console-consumer --topic PERSONS_WITH_ADDRESS --bootstrap-server localhost:9092 --from-beginning`
+See the results*: 
+
+```kafka-protobuf-console-consumer --topic PERSONS_BY_BIRTHYEAR --bootstrap-server localhost:9092 --from-beginning --property print.key=true --key-deserializer org.apache.kafka.common.serialization.IntegerDeserializer```
+
+*Requires Confluent CLI or kafka-protobuf-console-consumer to be installed.
--- rust-data-creator/src/ksqldb.rs
+++ rust-data-creator/src/ksqldb.rs
@@ -7,12 +7,7 @@ pub async fn create_streams_and_tables() {
     let ksql = KsqlDB::new(url, Client::builder(), false).unwrap();
     let query = r#"
     CREATE TABLE IF NOT EXISTS persons (id_key STRING PRIMARY KEY) WITH (KAFKA_TOPIC = 'persons', VALUE_FORMAT = 'PROTOBUF');
-    CREATE STREAM IF NOT EXISTS address_updates (country_id STRING KEY) WITH (KAFKA_TOPIC = 'address-updates', VALUE_FORMAT = 'PROTOBUF');
-    CREATE STREAM IF NOT EXISTS exploded_address_updates AS SELECT country_id, country, EXPLODE(persons) AS person_id, EXPLODE(addresses)
-    AS address FROM address_updates;
-    CREATE STREAM IF NOT EXISTS persons_with_address AS SELECT persons.id_key AS id, persons.first_name as first_name, persons.last_name
-    as last_name, persons.birthday as birthday, country, address FROM exploded_address_updates JOIN persons ON
-    exploded_address_updates.person_id = persons.id_key;
+    CREATE TABLE IF NOT EXISTS persons_by_birthyear AS SELECT BIRTHDAY->YEAR as BIRTHYEAR, COUNT(*) as TOTAL FROM persons GROUP BY BIRTHDAY->YEAR EMIT CHANGES;
     "#;
     match ksql.create(query, &Default::default(), None).await {
         Ok(r) => println!("Success creating table and streams: {:?}", r),
